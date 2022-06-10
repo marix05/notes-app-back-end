347eeb516a88bd7e4caf1ce20d64279e995f5e2d
Marix Marchellino Susanto <marixsusantooo@gmail.com>
2022-06-10 20:05:49 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+const { 
+  addNoteHandler, 
+  getAllNotesHandler, 
+  getNoteByIdHandler, 
+  editNoteByIdHandler,
+  deleteNoteByIdHandler, 
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => '<h1> Selamat datang di homepage </h1><br> <a href="http://localhost:5000/notes">Check All Note</a>', 
+  },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
